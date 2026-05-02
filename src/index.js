cf1f16f514dfde4de7ec61c7aa8dcb7eac6ccc6d
Marcos Vazquez <mmvazquez364@gmail.com>
2026-05-02 10:46:12 -0400

    html writeup for contact form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./home.css";
 import "./menu.css";
+import "./contact.css";
 import { Home } from "./home.js";
 import { Menu } from "./menu.js";
 
@@ -8,9 +9,10 @@ const content = document.querySelector("#content");
 const home = Home();
 const menu = Menu();
 
+/*
 content.appendChild(menu.displayHeading());
 content.appendChild(menu.displayMenu());
-/*
+
 content.appendChild(home.displayHeading());
 content.appendChild(home.displayGordonImages());
 content.appendChild(home.displayIntroPanel());
